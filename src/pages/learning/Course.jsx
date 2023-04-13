66d57f0066fb87d29d49f6285bb2f7798543c9b0
K0lumna <michaljaniec12@wp.pl>
2023-04-13 22:45:21 +0200

    added summary to each lesson

--- src/pages/learning/Course.jsx
+++ src/pages/learning/Course.jsx
@@ -211,59 +211,58 @@ function Course() {
                         />
                       );
 
-                    case "summary":
-                      return (
-                        <div className="bg-slate-300 rounded-xl flex justify-between gap-8 p-4">
-                          <div className="w-full flex flex-col gap-2">
-                            <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
-                              <span className="text-5xl text-slate-50">
-                                <MdQuiz />
-                              </span>
-                              <span className="text-2xl font-black text-slate-50">
-                                QUIZ
-                              </span>
-                            </div>
-                            <div className="w-full flex justify-center items-center p-2 bg-gray-500 font-bold text-slate-50 rounded-xl">
-                              NIE UKOŃCZONO
-                            </div>
-                          </div>
-                          <div className="w-full flex flex-col gap-2">
-                            <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
-                              <span className="text-5xl text-slate-50">
-                                <MdMovie />
-                              </span>
-                              <span className="text-2xl font-black text-slate-50">
-                                FILM
-                              </span>
-                            </div>
-                            <div className="w-full flex justify-center items-center p-2 bg-green-500 font-bold text-slate-50 rounded-xl">
-                              UKOŃCZONO
-                            </div>
-                          </div>
-                          <div className="w-full flex flex-col gap-2">
-                            <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
-                              <span className="text-5xl text-slate-50">
-                                <MdCode />
-                              </span>
-                              <span className="text-2xl font-black text-slate-50">
-                                ZADANIE
-                              </span>
-                            </div>
-                            <div className="w-full flex justify-center items-center p-2 bg-gray-500 font-bold text-slate-50 rounded-xl">
-                              NIE UKOŃCZONO
-                            </div>
-                          </div>
-                        </div>
-                      );
-
                     default:
                       return (
                         <p className="text-red-500 font-bold">
-                          Błędny typ paragrafu ({item.value})
+                          Błędny typ paragrafu "<strong>{item.type}</strong>"
                         </p>
                       );
                   }
                 })}
+                <div className="bg-slate-300 rounded-xl flex flex-col justify-between gap-4 p-4">
+                          <span className="text-center font-bold text-xl">PODSUMOWANIE</span>
+                          <section className="flex gap-8">
+                            <div className="w-full flex flex-col gap-2">
+                              <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
+                                <span className="text-5xl text-slate-50">
+                                  <MdQuiz />
+                                </span>
+                                <span className="text-2xl font-black text-slate-50">
+                                  QUIZ
+                                </span>
+                              </div>
+                              <div className="w-full flex justify-center items-center p-2 bg-gray-500 font-bold text-slate-50 rounded-xl">
+                                NIE UKOŃCZONO
+                              </div>
+                            </div>
+                            <div className="w-full flex flex-col gap-2">
+                              <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
+                                <span className="text-5xl text-slate-50">
+                                  <MdMovie />
+                                </span>
+                                <span className="text-2xl font-black text-slate-50">
+                                  FILM
+                                </span>
+                              </div>
+                              <div className="w-full flex justify-center items-center p-2 bg-green-500 font-bold text-slate-50 rounded-xl">
+                                UKOŃCZONO
+                              </div>
+                            </div>
+                            <div className="w-full flex flex-col gap-2">
+                              <div className="bg-slate-500 hover:bg-slate-600 btn-anim rounded-xl flex flex-col items-center gap-2 p-4">
+                                <span className="text-5xl text-slate-50">
+                                  <MdCode />
+                                </span>
+                                <span className="text-2xl font-black text-slate-50">
+                                  ZADANIE
+                                </span>
+                              </div>
+                              <div className="w-full flex justify-center items-center p-2 bg-gray-500 font-bold text-slate-50 rounded-xl">
+                                NIE UKOŃCZONO
+                              </div>
+                            </div>
+                          </section>
+                        </div>
             </section>
             <footer className="flex justify-end gap-4">
               {lesson && (
