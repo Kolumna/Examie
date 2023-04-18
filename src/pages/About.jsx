904fdec45b86260561a52f4167aa8309cd089b38
K0lumna <michaljaniec12@wp.pl>
2023-04-18 21:36:52 +0200

    visual fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,16 @@ import logo from "../assets/svgs/logo.svg";
 
 export default function About(props) {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <img width={360} src={logo} />
+    <div className="py-12 flex justify-center items-center">
+      <section className="about-us px-8">
+        <img className="rounded-xl" src="../src/assets/imgs/about.jpg" alt="" class="about-us__img" />
+        <div class="about-us__box">
+          <h3 className="font-black text-4xl mb-8">O NAS</h3>
+          <p class="text-xl">Examie to platforma edukacyjna obejmująca głównie zagadnienia związane z nauką wybranego zawodu w technikum. Aplikacja umożliwia uczenie się podstaw języków programowania wymaganego w danej klasyfikacji zawodowej, jak i rozwiązywania quizów z teorii z danej klasyfikacji i rozwiązywania egzaminów. Każdy użytkownik ma pokazany status przechodzonych kursów i statystyki z quizów bądź egzaminów. W przyszłości powstanie Examie Arena, która będzie polegała na rywalizacji graczy w obrębie nauki zawodu. </p>
+        </div>
+      </section>
     </div>
   );
 }
+
+
