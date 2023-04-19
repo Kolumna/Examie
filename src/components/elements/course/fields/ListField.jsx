fc7193555a3a92d30b6337e809e441e486201641
K0lumna <michaljaniec12@wp.pl>
2023-04-19 21:47:15 +0200

    course list visual fix

--- src/components/elements/course/fields/ListField.jsx
+++ src/components/elements/course/fields/ListField.jsx
@@ -1,11 +1,11 @@
 const ListField = (props) => {
   return (
-    <div>
+    <div className="bg-slate-100 p-8 rounded-xl">
       <h2 className="text-2xl mb-8 font-bold">{props.label}</h2>
-      <ul className="text-2xl font-semibold list-disc ml-14">
+      <ul className="text-2xl font-semibold flex flex-col gap-2">
         {props.value.map((item) => {
           return (
-            <li className="text-xl" key={item}>
+            <li className="text-xl bg-white p-4 rounded-xl" key={item}>
               {item}
             </li>
           );
